efbf3fc44761da50a7d09e54fc98e9c15a7fd3f4
Jimmy Arismendy Sanabria <jimmy@chiper.co>
2025-08-20 18:31:49 -0500

    Logic Concluded

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,17 +1,22 @@
+import ShortlinkPage from '@/components/ShortlinkPage';
+import { headers } from 'next/headers';
+
 interface ShortlinkRedirectionProps {
   params: Promise<{
     id: string;
   }>;
 }
 
-export default async function ShortlinkPage(
-  context: ShortlinkRedirectionProps,
-) {
+export default async function Page(context: ShortlinkRedirectionProps) {
   const { id } = await context.params;
+
+  const userAgent = (await headers()).get('user-agent') || '';
+
   return (
     <>
-      <h1>Shorlink present.</h1>
-      <div> Shorlink Id {id}</div>
+      {/* <h1>Shorlink present.</h1>
+      <div> Shorlink Id {id}</div> */}
+      <ShortlinkPage userAgent={userAgent} />
     </>
   );
 }
--- lib/config.ts
+++ lib/config.ts
@@ -5,9 +5,7 @@ const envSchema = Joi.object({
   FIREBASE_PROJECT_ID: Joi.string().required(),
   PORT: Joi.number().default(3000),
   NODE_ENV: Joi.string().valid('development', 'staging', 'production'),
-  IOS_SCHEMA_LINK: Joi.string().uri().required(),
-  IOS_UNIVERSAL_LINK: Joi.string().uri().required(),
-  IOS_STORE_URL: Joi.string().uri().required(),
+  IOS_SCHEMA_LINK: Joi.string().required(),
   IOS_STORE_URL_CHROME: Joi.string().uri().required(),
   IOS_TIMEOUT_MS: Joi.number().default(5000),
   ANDROID_APP_LINK_HOST: Joi.string().required(),
@@ -18,14 +16,11 @@ const envSchema = Joi.object({
   // Add more variables and validation rules as needed
 }).unknown();
 
-console.log({ env: process.env });
 const { value: envVars, error } = envSchema.validate(process.env, {
-  abortEarly: true,
+  abortEarly: false,
 });
 
-console.log('FIREBASE_SERVICE_ACCOUNT:', envVars.FIREBASE_SERVICE_ACCOUNT);
 if (error) {
-  console.log('Config validation error:', envVars);
   throw new Error(`Config validation error: ${error.message}`);
 }
 
@@ -51,7 +46,6 @@ export const appConfig = {
     installUrl: envVars.ANDROID_STORE_URL,
     packageName: envVars.ANDROID_PACKAGE_NAME,
     host: envVars.ANDROID_APP_LINK_HOST,
-    path: envVars.ANDROID_APP_LINK_PATH,
     timeout: envVars.ANDROID_TIMEOUT_MS,
   },
   // Add more as needed
